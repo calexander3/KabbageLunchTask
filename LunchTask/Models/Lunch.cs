8cd4bcfc77f40e77e428a1cf53694767c7e9b01c
Craig Alexander <calexander@kabbage.com>
2016-04-08 16:04:53 -0400

    downloading lunch data

---
+++ LunchTask/Models/Lunch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace LunchTask.Models
+{
+    public class Lunch
+    {
+
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+
+        [JsonProperty("menu")]
+        public string Menu { get; set; }
+
+        [JsonProperty("ratings")]
+        public IList<object> Ratings { get; set; }
+
+        [JsonProperty("comments")]
+        public IList<object> Comments { get; set; }
+
+        [JsonProperty("image")]
+        public string Image { get; set; }
+    }
+
+}
--- LunchTask/Program.cs
+++ LunchTask/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using LunchTask.Models;
+using Newtonsoft.Json;
 
 namespace LunchTask
 {
@@ -10,6 +14,35 @@ namespace LunchTask
     {
         static void Main(string[] args)
         {
+            var lunchRequest = WebRequest.CreateHttp("https://lunch.kabbage.com/api/v2/lunches");
+            var lunches = new Lunch[0];
+            var response = lunchRequest.GetResponse();
+            using (var responseStream = response.GetResponseStream())
+            {
+                if (responseStream != null)
+                {
+                    using (var reader = new StreamReader(responseStream))
+                    {
+                        lunches = JsonConvert.DeserializeObject<Lunch[]>(reader.ReadToEnd());
+                    }
+                }
+            }
+
+            foreach (var lunch in lunches)
+            {
+                if (lunch.Date > DateTime.Today)
+                {
+                    Console.WriteLine(lunch.Menu);
+                }
+            }
+
+            if (Environment.UserInteractive)
+            {
+                Console.Write("\nPress any key to continue...");
+                Console.ReadKey(true);
+                Console.WriteLine("\nExiting application...");
+                Environment.Exit(0);
+            }
         }
     }
 }
